8b0b7f6d011b307d78f13a6f60fa131ad7409d6a
KristineIgdao <kristineigdao1903@gmail.com>
2024-11-12 23:16:34 +0800

    log in update

--- src/components/EmailForgetPassword/index.js
+++ src/components/EmailForgetPassword/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { View, Text, TextInput, StyleSheet, Image } from 'react-native';
 
 const EmailForgetPassword = () => (
   <View style={styles.inputContainer}>
+    {/* Logo Image */}
+    <Image 
+      source={require('../../assets/Forgetpasswordlogo.png')} // Update this path based on your file structure
+      style={styles.logo} 
+    />
+
+    <Text style={styles.subtitle}>
+      Forgot Password?{'\n'}
+      Don't worry! It happens. Please enter the email address linked with your account.
+    </Text>
     <Text style={styles.title}>Enter email address</Text>
     <TextInput
       placeholder="Enter your email"
@@ -17,12 +27,25 @@ const styles = StyleSheet.create({
   inputContainer: {
     width: '100%',
     marginBottom: 15,
+    alignItems: 'center', 
+  },
+  logo: {
+    width: 300, 
+    height: 300, 
+    marginBottom: 40, 
+    resizeMode: 'contain', 
+  },
+  subtitle: {
+    fontSize: 14,
+    color: '#000',
+    textAlign: 'center',
+    marginBottom: 10,
   },
   title: {
-    fontSize: 16, 
-    fontWeight: 'bold', 
-    marginBottom: 15, 
-    textAlign: 'center', 
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 15,
+    textAlign: 'center',
   },
   input: {
     borderWidth: 1,
